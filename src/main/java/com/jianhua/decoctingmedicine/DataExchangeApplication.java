ae6d5c9f5ea2e7469c443a31a72956d099252baa
gyh <296891807@qq.com>
2024-03-18 14:54:20 +0800

    1、工程初始化提交

---
+++ src/main/java/com/jianhua/decoctingmedicine/DataExchangeApplication.java
@@ -0,0 +1,13 @@
+package com.jianhua.decoctingmedicine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataExchangeApplication.class, args);
+	}
+
+}
